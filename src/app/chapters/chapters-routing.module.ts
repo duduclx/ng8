636a9413fd4747fef722a9658e8d99a79614ddd8
dutillej <julien.dutilleul@univ-grenoble-alpes.fr>
2019-09-30 16:09:54 +0200

    chapitre 4

--- src/app/chapters/chapters-routing.module.ts
+++ src/app/chapters/chapters-routing.module.ts
@@ -5,13 +5,15 @@ import {ChapterOneComponent} from './chapter-one/chapter-one.component';
 import {ChapterTwoComponent} from './chapter-two/chapter-two.component';
 import {ChapterThreeComponent} from './chapter-three/chapter-three.component';
 import {ChapterThreeSuiteComponent} from './chapter-three-suite/chapter-three-suite.component';
+import {ChapterFourComponent} from './chapter-four/chapter-four.component';
 
 
 const routes: Routes = [
-  {path: 'home', component: ChapterThreeSuiteComponent},
+  {path: 'home', component: ChapterFourComponent},
   {path: 'composant', component: ChapterOneComponent},
   {path: 'module', component: ChapterTwoComponent},
-  {path: 'router', component: ChapterThreeComponent}
+  {path: 'router', component: ChapterThreeComponent},
+  {path: 'routerLink', component: ChapterThreeSuiteComponent}
 ];
 
 @NgModule({
--- src/app/chapters/chapters.module.ts
+++ src/app/chapters/chapters.module.ts
@@ -7,13 +7,15 @@ import { ChapterThreeComponent } from './chapter-three/chapter-three.component';
 
 import { ChaptersRoutingModule } from './chapters-routing.module';
 import { ChapterThreeSuiteComponent } from './chapter-three-suite/chapter-three-suite.component';
+import { ChapterFourComponent } from './chapter-four/chapter-four.component';
 
 @NgModule({
   declarations: [
     ChapterOneComponent,
     ChapterTwoComponent,
     ChapterThreeComponent,
-    ChapterThreeSuiteComponent
+    ChapterThreeSuiteComponent,
+    ChapterFourComponent
   ],
   imports: [
     CommonModule,
